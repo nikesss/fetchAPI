a3045b1d8e446b22d50fbd5741a5e74056d5d4be
nikesss <nike-super@bk.ru>
2021-02-09 16:10:54 +0300

    add

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+let f = document.getElementById('mark');
+let status_url;
+let array = [];
+let status_animal;
 document.getElementById('cat').addEventListener('click', () => {
+    document.querySelector('#new_animal').classList.add('list_mark');
+    f.style.opacity = '1';
     document.querySelector('.info').innerHTML = '';
-    fetch('https://api.thecatapi.com/v1/images/search')
+    qwer('https://api.thecatapi.com/v1/images/search');
+    status_animal = 'cat;'
+});
+
+document.getElementById('dog').addEventListener('click', () => {
+    document.querySelector('#new_animal').classList.add('list_mark');
+    f.style.opacity = '1';
+    document.querySelector('.info').innerHTML = '';
+    qwer('https://api.thedogapi.com/v1/images/search');
+    status_animal = "dog";
+});
+
+document.getElementById('like').addEventListener('click', () => {
+
+
+    array.push({ status: 2, link: status_url });
+    document.querySelector('.info').innerHTML = '';
+    f.style.opacity = '1';
+    if (status_animal == 'cat') {
+        qwer('https://api.thecatapi.com/v1/images/search');
+    } else {
+        qwer('https://api.thedogapi.com/v1/images/search');
+    }
+
+
+    console.log(array);
+})
+document.getElementById('dislike').addEventListener('click', () => {
+
+
+    array.push({ status: 1, link: status_url });
+    document.querySelector('.info').innerHTML = '';
+    f.style.opacity = '1';
+    if (status_animal == 'cat') {
+        qwer('https://api.thecatapi.com/v1/images/search');
+    } else {
+        qwer('https://api.thedogapi.com/v1/images/search');
+    }
+
+    console.log(array);
+})
+
+function qwer(link) {
+    fetch(link)
         .then((response) => {
             console.log(response)
             response.json().then(data => {
@@ -8,6 +57,7 @@ document.getElementById('cat').addEventListener('click', () => {
                     let img = document.createElement('img');
                     img.setAttribute('class', 'main_img');
                     img.src = data[0].url;
+                    status_url = data[0].url;
                     let p = document.createElement('p');
                     p.setAttribute('class', 'main_info');
                     document.querySelector('.info').appendChild(img);
@@ -34,43 +84,30 @@ document.getElementById('cat').addEventListener('click', () => {
             p.innerHTML = `Error is ${data}`;
             document.querySelector('.info').appendChild(p);
         })
+}
 
-})
-document.getElementById('dog').addEventListener('click', () => {
+document.getElementById('end_mark').addEventListener('click', () => {
+    f.style.opacity = '0';
     document.querySelector('.info').innerHTML = '';
-    fetch('https://api.thedogapi.com/v1/images/search')
-        .then((response) => {
-            console.log(response)
-            response.json().then(data => {
-                    console.log(data)
-                    let img = document.createElement('img');
-                    img.setAttribute('class', 'main_img');
-                    img.src = data[0].url;
-                    let p = document.createElement('p');
-                    p.setAttribute('class', 'main_info');
-                    document.querySelector('.info').appendChild(img);
-                    if (data[0].breeds[0]) {
+    document.querySelector('.list_mark').classList.remove('list_mark');
+    document.querySelector('#like_mark').innerHTML = '';
+    document.querySelector('#dislike_mark').innerHTML = '';
 
-                        p.innerHTML = `Name: ${data[0].breeds[0].name}, weight: imperial -  ${data[0].breeds[0].weight.imperial}, metric -  ${data[0].breeds[0].weight.metric}`
-                        document.querySelector('.info').appendChild(p);
-                    } else {
-                        p.innerHTML = `little information`;
-                        document.querySelector('.info').appendChild(p);
-                    }
-                })
-                .catch(data => {
+    for (let i = 0; i < array.length; i++) {
+        let li = document.createElement('li');
+        li.style.listStyle = 'none';
+        if (array[i].status == 2) {
+            let img = document.createElement('img');
+            img.src = array[i].link;
+            li.append(img);
 
-                    let p = document.createElement('p');
-                    p.setAttribute('class', 'main_info');
-                    p.innerHTML = `little information`;
-                    document.querySelector('.info').appendChild(p);
-                })
-        })
-        .catch(data => {
-            let p = document.createElement('p');
-            p.setAttribute('class', 'main_info');
-            p.innerHTML = `Error is ${data}`;
-            document.querySelector('.info').appendChild(p);
-        })
+            document.querySelector('#like_mark').appendChild(li);
+        } else {
+            let img = document.createElement('img');
+            img.src = array[i].link;
+            li.append(img);
 
+            document.querySelector('#dislike_mark').appendChild(li);
+        }
+    }
 })
